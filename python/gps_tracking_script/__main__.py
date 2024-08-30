d72e51ee2bd7f5cc7ef1068125a90f08022054b5
FlowingSkiff <boats303@gmail.com>
2024-08-29 21:17:01 -0600

    cli and argparse

--- python/gps_tracking_script/__main__.py
+++ python/gps_tracking_script/__main__.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env
 import logging
-import pathlib
+
+from gps_tracking_script.cli import parse_args
 from gps_tracking_script.parse import parse_test_file
 
 logging.basicConfig(level=logging.DEBUG)
-
-parse_test_file(pathlib.Path.cwd() / "sample_data.txt")
+args = parse_args()
+parse_test_file(args.input_file)
